428225c2d7593ab8182623191d2b3cc94b960061
sidhardha <sidhardha.ganji@gmail.com>
2025-09-19 13:32:18 +0300

    conversation dialogues

--- conversation.sh
+++ conversation.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-echo "Ramesh:: Hi Suresh, how are you?"
-echo "Suresh:: Hello Ramesh, I'm fine, how are you?"
-echo "Ramesh:: What are you doing these days?"
-echo "Suresh:: I'm learning shell scripting"
+
+#declaring variables
+PERSON1=Sidhu
+PERSON2=Swathi
+
+echo "$PERSON1:: Hi $PERSON2, how are you?"
+echo "$PERSON2:: Hello $PERSON1, I'm fine, how are you?"
+echo "$PERSON1:: What are you doing these days?"
+echo "$PERSON2:: I'm learning shell scripting"
