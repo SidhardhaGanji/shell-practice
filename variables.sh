9a066e37d9a7f31b7d9e58bcf35e85967b63063a
sidhardha <sidhardha.ganji@gmail.com>
2025-09-19 20:13:19 +0300

    variable read

--- variables.sh
+++ variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
+#Passing arguments dynamically#
 PERSON1=$1
 PERSON2=$2
 
+#Display conversation by passing arguments dynamically#
 echo "$PERSON1:: Welcom to Alaska $PERSON2"
 echo "$PERSON2:: Thank you $PERSON1, Finally not on zoom"
