ae98118d22014f99e6dc5edde1f3b8d2cad9793e
sidhardha <sidhardha.ganji@gmail.com>
2025-09-19 13:15:59 +0300

    conversation dialogues

---
+++ conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Ramesh:: Hi Suresh, how are you?"
+echo "Suresh:: Hello Ramesh, I'm fine, how are you?"
+echo "Ramesh:: What are you doing these days?"
+echo "Suresh:: I'm learning shell scripting"
