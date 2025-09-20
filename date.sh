ebd369c077d71b944a7ca69c1b8173fb79bab9bc
sidhardha <sidhardha.ganji@gmail.com>
2025-09-20 21:05:15 +0300

    Remove date.sh from repo

--- date.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash          # Shebang → tells Linux to use Bash interpreter
-
-DATE=$(date)         # Runs the `date` command and stores its output in variable DATE
-
-echo "Time stamp executed: $DATE"   # Prints the message with the current date/time
