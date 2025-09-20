de5003fdb21def0f3bc71ce8893941f82c2607de
sidhardha <sidhardha.ganji@gmail.com>
2025-09-20 20:50:28 +0300

    Save local changes before rebase

--- date.sh
+++ date.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/bin/bash          # Shebang → tells Linux to use Bash interpreter
 
-DATE=$(date)
+DATE=$(date)         # Runs the `date` command and stores its output in variable DATE
 
-echo "Time stamp executed: $DATE"
+echo "Time stamp executed: $DATE"   # Prints the message with the current date/time
